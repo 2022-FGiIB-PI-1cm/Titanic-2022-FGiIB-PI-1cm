433f75728434304de691c71a7b5817752befd794
2022-FGiIB-PI-1cm <ptclnyhab@rambler.ru>
2023-05-13 04:01:52 +0700

    Фиксирую изменения в файле main.py

--- variant_number_2_banaev.py
+++ variant_number_2_banaev.py
@@ -1,16 +1,15 @@
-# Вячеслав Белинский группа 2022-ФГиИБ-ПИ-1см
-# Вариант № 3
+# Юрий Банаев группа 2022-ФГиИБ-ПИ-1см
+# Вариант № 2
 
 import streamlit as st
 
-with open("data.csv") as file:
-    next(file);
-    for row in file:
-        rows_data = file.readlines();
+with open(r"C:\Users\Cybernetics\PycharmProjects\Лабораторные_работы\laboratory_work_12\data.csv") as file:
+    data = file.readlines();
 
-def getting_a_list_of_mens(rows_data, min_value_age, max_value_age, gender_of_the_person):
-    if (st.button("Найти")):
-     for row in rows_data:
-        if row.split(',')[6]>=min_value_age and row.split(',')[6]<=max_value_age and row.split(',')[5]==gender_of_the_person:
-            st.text (row.rstrip().split(',')[3] + ' - ' + row.rstrip().split(',')[4] + ' - ' + row.rstrip().split(',')[
-            6] + ' - ' + row.rstrip().split(',')[2]);
+
+def get_pass_list(data, save, sex):
+    out_list = []
+    for line in data:
+        if line.split(',')[1] == save and line.split(',')[5] == sex:
+            out_list += [line]
+    return out_list
