c8b2a2e79b07a6c0cfb561b68217e32792754cf9
2022-FGiIB-PI-1cm <ptclnyhab@rambler.ru>
2023-05-19 18:48:07 +0700

    Фиксирую изменения в файле main.py

--- test_variant_number_2_banaev.py
+++ test_variant_number_2_banaev.py
@@ -1,12 +1,12 @@
 # Тесты Банаева Ю. А.
 from variant_number_2_banaev import get_pass_list
 
+
 def test_pass_list_one1():
     data = ['0,1,2,3,4,female,1', '1,0,2,3,4,male,1', '2,1,2,3,4,male,1']
     assert get_pass_list(data, '1', 'male') == {'Возраст': [['1']], 'Имя': [['34']], 'Класс билета': [['2']]}
 
+
 def test_pass_list_one2():
     data = ['0,1,2,3,4,female,1', '1,0,2,3,4,male,1', '2,1,2,3,4,male,3']
     assert get_pass_list(data, '1', 'male') == {'Возраст': [['3']], 'Имя': [['34']], 'Класс билета': [['2']]}
-
-
