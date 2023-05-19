578c5e1ccca01d100fad3b981195a2ec5686b693
2022-FGiIB-PI-1cm <ptclnyhab@rambler.ru>
2023-05-19 18:43:30 +0700

    Фиксирую изменения в файле main.py

--- test_variant_number_3_belinskiy.py
+++ test_variant_number_3_belinskiy.py
@@ -14,6 +14,7 @@ def test_getting_list_mens_1():
                                                                 '"Kent  Mr. Edward Austin", 58, 1',
                                                                 '"Newell  Mr. Arthur Webster", 58, 1']
 
+
 def test_getting_list_mens_2():
     rows_data = ['1,1,1,"Frolicher-Stehli, Mr. Maxmillian",male,60',
                  '2,0,2,"Brown, Mr. Thomas William Solomon",male,60',
@@ -22,6 +23,7 @@ def test_getting_list_mens_2():
     assert getting_list_mens (rows_data, '60', '60', 'male') == ['"Frolicher-Stehli  Mr. Maxmillian", 60, 1',
                                                                  '"Brown  Mr. Thomas William Solomon", 60, 2',
                                                                  '"Weir  Col. John", 60, 1']
+
 
 def test_getting_list_mens_3():
     rows_data = ['1,1,1,"Frolicher-Stehli, Mr. Maxmillian",male,60',
